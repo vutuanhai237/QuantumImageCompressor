d01331f607f08c55ceead6b67f8d85d0d3eddf08
Vu Tuan Hai <haimeohung@gmail.com>
2025-02-06 17:23:44 +0900

    Add block class

--- utils.py
+++ utils.py
@@ -1,4 +1,7 @@
 import numpy as np
+import pennylane as qml
+import pennylane.numpy as nps
+import qiskit
 
 def divide_image(img, k):
     # Add padding if needed
@@ -24,3 +27,59 @@ def divide_image(img, k):
             blocks.append(block)
             scales.append(norm)
     return blocks, scales
+
+dev = qml.device("default.qubit")
+
+def state_to_qc(state: np.ndarray) -> qiskit.QuantumCircuit:
+    num_qubits = int(np.log2(state.shape[0]))
+    qc = qiskit.QuantumCircuit(num_qubits)
+    qc.prepare_state(state)
+    qcx = qiskit.transpile(qc, basis_gates=['h','s','cx','u','rx','ry','rz'], optimization_level=3)
+    return qcx
+
+
+
+def circuit_curry(qcx, num_qubits, num_layers):
+    @qml.qnode(dev, diff_method="parameter-shift")
+    def circuit(thetas):
+        k = 0
+        for _ in range(num_layers):
+            for i in range(0, num_qubits - 2):
+                qml.CNOT(wires = [i, i+1])
+            qml.CNOT(wires = [num_qubits - 1, 0])
+            for i in range(0, num_qubits - 1):
+                qml.RX(thetas[k], wires = i)
+                qml.RY(thetas[k+1], wires = i)
+                qml.RZ(thetas[k+2], wires = i)
+                k += 3
+        qml.from_qiskit(qcx)
+        return qml.expval(qml.PauliZ(0))
+    return circuit
+    
+def cost_curry(circuit):
+    def cost_func(thetas):
+        return 1 - circuit(thetas)
+    return cost_func
+
+
+def compilation(state, thetas, num_layers, steps = 20, opt = qml.AdamOptimizer(stepsize = 0.1)):
+    # Thetas must be nps array
+    qcx = state_to_qc(state)
+    num_qubits = int(np.log2(state.shape[0]))
+    circuit = circuit_curry(qcx, num_qubits, num_layers)
+    cost_func = cost_curry(circuit)
+    grad_func = qml.grad(cost_func)
+
+    costs = []
+    thetass = []
+    if thetas is None:
+        thetas = nps.array(np.ones(3*num_layers*num_qubits))
+    print(thetas.shape)
+    for i in range(steps):
+        thetas, prev_cost = opt.step_and_cost(cost_func, thetas, grad_fn = grad_func)
+        if prev_cost < 10^(-4):
+            print(f"Achieved error threshold at step {i}")
+            break
+        costs.append(prev_cost)
+        thetass.append(thetas)
+    return costs, thetass
